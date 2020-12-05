c9428b9e77f0993420298ff8585a18b2784f920f
Tajah Johnson <tajah93@hotmail.com>
2020-12-05 17:54:32 -0600

    Created routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
-import Banner from "./components/Banner/Banner"; 
+import Banner from "./components/Banner/Banner";
 import Form from "./components/Form/Form";
+
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <Router>
 
-      <Form />
-      
-        
-        
-      
+
+        <Switch>
+          <Route path="/saved">
+            {/* saved components go here */}
+          </Route>
+          <Route path="/">
+            {/* search componenets go here */}
+            <Form />
+          </Route>
+        </Switch>
+
+      </Router>
+
+
     </div>
   );
 }
