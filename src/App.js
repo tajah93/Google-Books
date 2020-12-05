8f9040e1b1e26e9127f8b80c49b47d0ddb0515b2
Tajah Johnson <tajah93@hotmail.com>
2020-12-05 17:45:59 -0600

    Imported Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./components/Header/Header";
 import Banner from "./components/Banner/Banner"; 
-
+import Form from "./components/Form/Form";
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+
+      <Form />
       
         
         
