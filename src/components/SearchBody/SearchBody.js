bc097c2b616ab422eedde6278818e2330a3bd75e
Tajah Johnson <tajah93@hotmail.com>
2020-12-05 19:44:03 -0600

    Created SearchBody card

---
+++ src/components/SearchBody/SearchBody.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SearchBody({ children }) {
+    return (
+        <div className="card">
+  <div className="card-body">
+    {children}
+  </div>
+</div>
+    );
+}
+
+export default SearchBody;
